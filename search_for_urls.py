1f79955c20c6a8727e27909bd41623f7086ed2ed
cyph1x <42013718+Cyph1x@users.noreply.github.com>
2024-10-16 23:05:22 +1000

    Changed search_for_urls.py to use proxies

--- search_for_urls.py
+++ search_for_urls.py
@@ -3,12 +3,13 @@ import os
 import bs4
 from enum import StrEnum
 import json
-
+import concurrent.futures
 
 class ProxyHandler:
 
-    def __init__(self, enable_http: bool = True, enable_socks4: bool = False, enable_socks5: bool = False):
+    def __init__(self, enable_http: bool = True, enable_https: bool = True, enable_socks4: bool = True, enable_socks5: bool = True):
         self.enable_http = enable_http
+        self.enable_https = enable_https
         self.enable_socks4 = enable_socks4
         self.enable_socks5 = enable_socks5
 
@@ -20,6 +21,15 @@ class ProxyHandler:
         socks4_proxies_url = "https://github.com/monosans/proxy-list/raw/refs/heads/main/proxies/socks4.txt"
         socks5_proxies_url = "https://github.com/monosans/proxy-list/raw/refs/heads/main/proxies/socks5.txt"
 
+        #http_proxies_url = "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt"
+        #socks4_proxies_url = "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt"
+        #socks5_proxies_url = "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt"
+
+        #http_proxies_url = "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/main/proxies/http.txt"
+        https_proxies_url = "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/main/proxies/https.txt"
+        #socks4_proxies_url = "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/main/proxies/socks4.txt"
+        #socks5_proxies_url = "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/main/proxies/socks5.txt"
+
         if self.enable_http:
             response = requests.get(http_proxies_url)
             if response.status_code != 200:
@@ -29,6 +39,16 @@ class ProxyHandler:
                 assert response.status_code == 200, "Failed to fetch HTTP proxies"
             self.http_proxies = response.text.split("\n")
             print(f"HTTP Proxies: {len(self.http_proxies)}")
+
+        if self.enable_https:
+            response = requests.get(https_proxies_url)
+            if response.status_code != 200:
+                print("Failed to fetch HTTPS proxies")
+                print(response.status_code)
+                print(response.text)
+                assert response.status_code == 200, "Failed to fetch HTTPS proxies"
+            self.https_proxies = response.text.split("\n")
+            print(f"HTTPS Proxies: {len(self.https_proxies)}")
 
         if self.enable_socks4:
             response = requests.get(socks4_proxies_url)
@@ -51,12 +71,27 @@ class ProxyHandler:
             print(f"SOCKS5 Proxies: {len(self.socks5_proxies)}")
 
     def get_proxy(self, protocol: str):
-        if protocol == "http" and self.enable_http:
-            return self.http_proxies.pop()
-        elif protocol == "socks4" and self.enable_socks4:
-           return self.socks4_proxies.pop()
-        elif protocol == "socks5" and self.enable_socks5:
-           return self.socks5_proxies.pop()
+
+        if protocol == "http" and self.enable_http and len(self.http_proxies) > 0:
+            proxy = self.http_proxies.pop()
+            while proxy == "" and len(self.http_proxies) > 0:
+                proxy = self.http_proxies.pop()
+            return proxy
+        elif protocol == "https" and self.enable_https and len(self.https_proxies) > 0:
+            proxy = self.https_proxies.pop()
+            while proxy == "" and len(self.https_proxies) > 0:
+                proxy = self.https_proxies.pop()
+            return proxy
+        elif protocol == "socks4" and self.enable_socks4 and len(self.socks4_proxies) > 0:
+            proxy = self.socks4_proxies.pop()
+            while proxy == "" and len(self.socks4_proxies) > 0:
+                proxy = self.socks4_proxies.pop()
+            return proxy
+        elif protocol == "socks5" and self.enable_socks5 and len(self.socks5_proxies) > 0:
+            proxy = self.socks5_proxies.pop()
+            while proxy == "" and len(self.socks5_proxies) > 0:
+                proxy = self.socks5_proxies.pop()
+            return proxy
         else:
             return None
 
@@ -102,32 +137,67 @@ class APKCombo:
         ZH = "zh"
 
     @staticmethod
-    def search(retries: int = 50):
-        if retries <= 0:
-            raise Exception("Failed to search for apks")
-        url = f"https://apkcombo.com/my-7-eleven/au.com.fuel7eleven/download/apk"
-        proxy = proxies.get_proxy("http")
-        print(f"Searching with proxy {proxy}")
-        response = requests.get(url, proxies={"http": proxy})
-        # response = requests.get(url)
-        if response.status_code != 200:
-            print("Failed to fetch APKCombo search results")
-            print(response.status_code)
-            print(response.text)
-            print(f"Retrying... {retries} left")
-            return APKCombo.search(retries-1)
-        soup = bs4.BeautifulSoup(response.text, "html.parser")
-        apks = []
-        # find all hrefs
-        for a in soup.find_all("a", href=True):
-            if "download.apkcombo.com" in a["href"]:
-                apks.append(a["href"])
-        # verify that only 7 eleven apks are returned
-        apks = [apk for apk in apks if "au.com.fuel7eleven" in apk]
-        if len(apks) == 0:
-            print(f"No APKs found, retrying... {retries} left")
-            return APKCombo.search(retries-1)
-        return apks
+    def fetch_with_proxy(proxy_type: str, proxy: str, url: str):
+        selected_proxies = {
+            "http": f"{proxy_type}://{proxy}",
+            "https": f"{proxy_type}://{proxy}"
+        }
+
+        try:
+            response = requests.get(url, proxies=selected_proxies, timeout=5)
+            if response.status_code == 200:
+                soup = bs4.BeautifulSoup(response.text, "html.parser")
+                apks = []
+                # find all hrefs
+                for a in soup.find_all("a", href=True):
+                    if "download.apkcombo.com" in a["href"]:
+                        apks.append(a["href"])
+                # verify that only 7 eleven apks are returned
+                apks = [apk for apk in apks if "au.com.fuel7eleven" in apk]
+                if apks:
+                    print(f"Found APKs with proxy {proxy}: {apks}")
+                    return apks
+            print(f"Failed with proxy {proxy}, status code: {response.status_code}")
+        except requests.exceptions.RequestException as e:
+            print(f"Error with proxy {proxy}: {e}")
+        return None
+
+    @staticmethod
+    def search_concurrently(proxies_list, url: str, max_workers: int = 20):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+            future_to_proxy = {
+                executor.submit(APKCombo.fetch_with_proxy, proxy_type, proxy, url): proxy
+                for proxy_type, proxy in proxies_list
+            }
+            for future in concurrent.futures.as_completed(future_to_proxy):
+                result = future.result()
+                if result:
+                    # kill all other threads
+                    for f in future_to_proxy:
+                        f.cancel()
+                    executor.shutdown(wait=False, cancel_futures=True)
+
+                    return result
+        raise Exception("Unable to search APK Combo")
+
+    @staticmethod
+    def search():
+        proxies_list=[]
+        while len(proxies.http_proxies) > 0 or len(proxies.https_proxies) > 0 or len(proxies.socks4_proxies) > 0 or len(proxies.socks5_proxies) > 0:
+            http_proxy = proxies.get_proxy("http")
+            if http_proxy:
+                proxies_list.append(("http", http_proxy))
+            https_proxy = proxies.get_proxy("https")
+            if https_proxy:
+                proxies_list.append(("https", https_proxy))
+            socks4_proxy = proxies.get_proxy("socks4")
+            if socks4_proxy:
+                proxies_list.append(("socks4", socks4_proxy))
+            socks5_proxy = proxies.get_proxy("socks5")
+            if socks5_proxy:
+                proxies_list.append(("socks5", socks5_proxy))
+        return APKCombo.search_concurrently(proxies_list, "https://apkcombo.com/my-7-eleven/au.com.fuel7eleven/download/apk")
+
 
 
 if __name__ == "__main__":
